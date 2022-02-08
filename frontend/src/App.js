b20a4c7c60da9cce266881478ece0d34b70e92b2
Charles Woods <charles.woods48@yahoo.com>
2022-02-08 11:59:03 -0600

    making more routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import SignupFormPage from './components/SignupFormPage';
 import * as sessionActions from './store/session'
 import Navigation from './components/Navigation'
 import Cocktails from './components/Cocktails'
+import CocktailForm from './components/Cocktails/newCocktail';
 
 function App() {
   const dispatch = useDispatch();
@@ -28,6 +29,9 @@ function App() {
           </Route>
           <Route path='/cocktails'>
             <Cocktails />
+          </Route>
+          <Route path='/cocktails/new'>
+            <CocktailForm/>
           </Route>
         </Switch>
       )}
--- frontend/src/store/cocktails.js
+++ frontend/src/store/cocktails.js
@@ -1,26 +1,34 @@
 
 const GET_ALL_COCKTAILS = 'cocktail/getAllCocktails';
 const CREATE_COCKTAIL = 'cocktail/createCocktail'
+const SINGLE_COCKTAIL = 'cocktail/singleCocktail'
 
 const fetchCocktails = (cocktails) => {
     return {
-        type:GET_ALL_COCKTAILS,
+        type: GET_ALL_COCKTAILS,
         cocktails
     };
 };
 
 const createNewCocktail = (cocktail) => {
+    return {
+        type: CREATE_COCKTAIL,
+        cocktail
+    }
+}
+
+const singleCocktail = (cocktail) => {
     return{
-        type:CREATE_COCKTAIL,
+        type: SINGLE_COCKTAIL,
         cocktail
     }
 }
 
 //thunk for all
-export const getCocktails = () => async(dispatch) => {
+export const getCocktails = () => async (dispatch) => {
     const response = await fetch('/api/cocktails');
 
-    if(response.ok){
+    if (response.ok) {
         const cocktails = await response.json();
 
         dispatch(fetchCocktails(cocktails))
@@ -28,15 +36,26 @@ export const getCocktails = () => async(dispatch) => {
     }
 }
 
+//thunk for a single cocktail
+export const getCocktail = (cocktailId) => async (dispatch) => {
+    const response = await fetch(`/api/cocktails/${cocktailId}`)
+    
+    if(response.ok) {
+        const cocktail = await response.json();
+        dispatch(singleCocktail(cocktail))
+    }
+    return response
+}
+
 //thunk to add new
-export const createCocktail = (cocktail) => async(dispatch) => {
-    const response = await fetch('/api/cocktails/new',{
+export const createCocktail = (cocktail) => async (dispatch) => {
+    const response = await fetch('/api/cocktails/new', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(cocktail)
     })
 
-    if(response.ok){
+    if (response.ok) {
         const cocktail = await response.json();
         dispatch(createNewCocktail(cocktail))
     }
@@ -44,17 +63,21 @@ export const createCocktail = (cocktail) => async(dispatch) => {
 }
 
 
-const initialState = {cocktails:{}}
+const initialState = { cocktails: {} }
 
 //cocktailReducer
 const cocktailReducer = (state = initialState, action) => {
-    switch (action.type){
-        case GET_ALL_COCKTAILS:{
-            const newState = {...state}
+    switch (action.type) {
+        case GET_ALL_COCKTAILS: {
+            const newState = { ...state }
             const cocktailList = {}
-            action.cocktails.forEach((cocktail)=> (cocktailList[cocktail.id] = cocktail))
+            action.cocktails.forEach((cocktail) => (cocktailList[cocktail.id] = cocktail))
             newState.cocktails = cocktailList
             return newState;
+        }
+        case CREATE_COCKTAIL:{
+            const newState = {...state}
+
         }
         default:
             return state
