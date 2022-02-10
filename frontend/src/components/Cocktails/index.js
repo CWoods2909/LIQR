555c03bf4c7d4b64fce69fba63812fa56b40e5b5
Charles Woods <charles.woods48@yahoo.com>
2022-02-09 18:19:17 -0600

    edit/delete feature

--- frontend/src/components/Cocktails/index.js
+++ frontend/src/components/Cocktails/index.js
@@ -1,19 +1,23 @@
 import React, { useEffect } from 'react';
-// import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-// import { Redirect } from 'react-router-dom';
+import { NavLink,Redirect } from 'react-router-dom';
 import { getCocktails } from '../../store/cocktails';
 import './allcocktails.css'
 
 const AllCocktails = () => {
     const dispatch = useDispatch();
     const cocktails = useSelector((state) => Object.values(state.cocktail.cocktails))
+    const sessionUser = useSelector((state) => state.session.user)
     
-
     useEffect(() => {
         dispatch(getCocktails())
     }, [dispatch]);
+
+    if (!sessionUser) return (
+        <Redirect to='/' />
+    )
+
+
 
     return (
         <>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,10 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+// import barImage from '../../images/bar-image.jpg'
+// import longIsland from '../../images/long-island.jpg'
+// import marg from '../../images/margarita.jpg'
+// import mojito from '../../images/mojito.jpg'
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
@@ -11,6 +15,7 @@ function Navigation({ isLoaded }) {
     if (sessionUser) {
         sessionLinks = (
             <ProfileButton user={sessionUser} />
+
         );
     } else {
         sessionLinks = (
@@ -22,13 +27,30 @@ function Navigation({ isLoaded }) {
     }
 
     return (
-        <div className="nav-bar-container">
+        <>
+            <div className="nav-bar-container">
                 <NavLink exact to="/">Home</NavLink>
-                {isLoaded && sessionLinks}
-                <NavLink to='/cocktails'>Cocktails</NavLink>
-                <NavLink to='/cocktails/new'>Add Cocktail</NavLink>
-                
-        </div>
+                <h2 className='nav-bar-header'>LIQR</h2>
+                <div className="nav-bar-right">
+                    {isLoaded && sessionLinks}
+                    <NavLink to='/cocktails'>Cocktails</NavLink>
+                    <NavLink to='/cocktails/new'>Add Cocktail</NavLink>
+                </div>
+            </div>
+            {/* <div className='image-container'>
+            <div className="image" style={{ backgroundImage: `url(${barImage})` }} />
+            </div> */}
+            {/* <div className='image-container'>
+                <div className='image' style={{ backgroundImage: `url(${longIsland})` }} />
+
+
+                <div className='image' style={{ backgroundImage: `url(${marg})` }} />
+
+
+                <div className='image' style={{ backgroundImage: `url(${mojito})` }} />
+
+            </div> */}
+        </>
     );
 }
 
